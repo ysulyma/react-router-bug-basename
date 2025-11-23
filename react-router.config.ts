19c271179f94be967614f350ea6ea67f9ef09c0a
Yuri Sulyma <yuri@liqvidjs.org>
2025-11-23 14:50:21 -0700

    change files

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
-  // Config options...
-  // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: true,
+	basename: "/test/",
+	prerender: true,
+	ssr: false,
 } satisfies Config;
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+	base: "/test/",
+	plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
